f2329bde023ad0a327382ce4ff44be5a8a2fb764
jeremyblow <jeremy@incerasolutions.com>
2020-10-25 13:02:17 -0700

    Bump tf req ver to `>= 0.13`. Change source ref for terraform-aws-modules/security-group/aws. Update hashicorp/aws ver constraint to `~> 3.12`.

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = "~> 0.12.24"
+  required_version = ">= 0.13"
 
   required_providers {
-    aws      = "~> 2.58.0"
+    aws      = "~> 3.12"
     template = "~> 2.1.2"
   }
 }
